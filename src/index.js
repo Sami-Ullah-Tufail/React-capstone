e25e8d9aaba6e529f379793db29e77f63ea9563f
samiullahtufail <sami.u.tufail@samiullahtufails-MacBook-Pro.local>
2023-10-25 18:45:53 +0500

    the store setup

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import AirPollutionApp from './App/AirPollutionApp';
 import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import worker from './Mocks/browser';
+import store from './App/store';
+import Home from './Containers/Home/Home';
+import Region from './Containers/Region/Region';
+import PollutionDetails from './Containers/PollutionDetails/PollutionDetails';
+
+if (process.env.REACT_APP_STAGE === 'development') {
+  worker.start();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AirPollutionApp />}>
+            <Route index element={<Home />} />
+            <Route path=":regionName" element={<Region />} />
+            <Route
+              path=":regionName/:country/:lat/:lon"
+              element={<PollutionDetails />}
+            />
+          </Route>
+          <Route path="*" element={<div>Not found</div>} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
